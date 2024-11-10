b2335a02f03eb8cd73b82c6ca1a4e3434df6b686
Aiswarya2316 <aiswaryapinkley@gmail.com>
2024-11-10 20:41:23 +0530

    home

--- jewels/app/views.py
+++ jewels/app/views.py
@@ -123,7 +123,10 @@ def shopregister(req):
 def userhome(req):
     if 'user' in req.session:
         data = Product.objects.all().order_by('-shop')[:4]
-        return render(req,'user/userhome.html',{'data':data})
+        data1 = Buy.objects.filter(user=get_usr(req)).order_by('-date_of_buying')[:2]  # Only get the latest 2 orders
+        data2 = cart.objects.filter(user=get_usr(req)).order_by('-id')[:2]  # Get the latest 4 cart items
+
+        return render(req,'user/userhome.html',{'data':data,'data1':data1,'data2':data2})
     else:
         return redirect(login)
 
