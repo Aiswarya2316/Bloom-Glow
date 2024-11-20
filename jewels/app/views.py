037314e684f393b4caae35503bc345e3f9d83670
Aiswarya2316 <aiswaryapinkey@gmail.com>
2024-11-20 10:31:36 +0530

    PROFILE UPDATE

--- jewels/app/views.py
+++ jewels/app/views.py
@@ -199,17 +199,27 @@ def profile(req):
 ###profile update
 def upload(req):
     if 'user' in req.session:
-        data=Register.objects.get(Email=req.session['user'])
-        if req.method=='POST':
-            name=req.POST['name']
-            phonenumber=req.POST['phonenumber']
-            location=req.POST['location']
-            Register.objects.filter(Email=req.session['user']).update(name=name,phonenumber=phonenumber,location=location)
+        try:
+            data = Register.objects.get(Email=req.session['user'])
+        except Register.DoesNotExist:
+            return redirect(login)
+
+        if req.method == 'POST':
+            name = req.POST['name']
+            phonenumber = req.POST['phonenumber']
+            location = req.POST['location']
+            if not re.match(r'^[789]\d{9}$', phonenumber):
+                return render(req, 'updateprofile.html', {
+                    'data': data,
+                    'error_message': 'Invalid phone number'
+                })
+            Register.objects.filter(Email=req.session['user']).update(name=name, phonenumber=phonenumber, location=location)
             return redirect(profile)
-        return render(req,'user/updateprofile.html',{'data':data})
+        return render(req, 'updateprofile.html', {'data': data})
 
     else:
-       return redirect(login)
+
+        return redirect(login)
     
 def userviewproduct(req):
     data=Product.objects.all()
